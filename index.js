9ef94d9bd1415a0a0dfe68c985f406174dc7a9f8
renan-dias <renanprodias2050@gmail.com>
2019-03-25 13:39:31 -0300

    alteração no escopo

--- index.js
+++ index.js
@@ -1,38 +1,18 @@
-const readline = require('readline-sync')
+
 const robots = {
-  text: require('./robots/text.js')
+  input: require('./robots/input.js'),
+  text: require('./robots/text.js'),
+  state: require('./robots/state.js'),
+  image: require('./robots/image.js')
 }
 
 async function start() {
-  const content = {
-    maximumSentences: 7
-  }
+  robots.input()
+  await robots.text()
+  await robots.image()
 
-  content.searchTerm = askAndReturnSearchTerm()
-  content.prefix = askAndReturnPrefix()
-  content.lang = askAndReturnLanguage()
-
-  await robots.text(content)
-
-  function askAndReturnSearchTerm() {
-    return readline.question('Type a Wikipedia search term: ')
-  }
-
-  function askAndReturnPrefix() {
-    const prefixes = ['Who is', 'What is', 'The history of']
-    const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
-    const selectedPrefixText = prefixes[selectedPrefixIndex]
-
-    return selectedPrefixText
-  }
-  function askAndReturnLanguage(){
-    const language = ['pt','en']
-    const selectedLangIndex = readline.keyInSelect(language,'Choice Language: ')
-    const selectedLangText = language[selectedLangIndex]
-    return selectedLangText
-  }
-
-  console.log(content)
+  const content = robots.state.load()
+  console.dir(content, { depth: null })
 }
 
 start()
--- robots/text.js
+++ robots/text.js
@@ -5,26 +5,29 @@ const sentenceBoundaryDetection = require('sbd')
 const watsonApiKey = require('../credentials/watson_nlu.json').apikey
 const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
  
-var nlu = new NaturalLanguageUnderstandingV1({
+const nlu = new NaturalLanguageUnderstandingV1({
   iam_apikey: watsonApiKey,
   version: '2018-04-05',
   url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
 })
 
-async function robot(content) {
+const state = require('./state.js')
+
+async function robot() {
+  const content = state.load()
+
   await fetchContentFromWikipedia(content)
   sanitizeContent(content)
   breakContentIntoSentences(content)
   limitMaximumSentences(content)
   await fetchKeywordsOfAllSentences(content)
 
+  state.save(content)
+
   async function fetchContentFromWikipedia(content) {
     const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
     const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
-    const wikipediaResponse = await wikipediaAlgorithm.pipe({
-      "lang" : content.lang,
-      "articleName": content.searchTerm
-    })
+    const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm)
     const wikipediaContent = wikipediaResponse.get()
 
     content.sourceContentOriginal = wikipediaContent.content
